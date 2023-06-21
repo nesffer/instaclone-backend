357ae1b4796d80f3f34c25dd721cf4b4e0634157
Nesffer <nesffer.jimin@gmail.com>
2023-06-21 23:37:59 +0900

    feat: Movie 정의, CRUD 구현

--- src/server.ts
+++ src/server.ts
@@ -1,38 +1,62 @@
+import { PrismaClient } from "@prisma/client";
 import { ApolloServer, gql } from "apollo-server";
+
+const client = new PrismaClient();
+
+interface Movie {
+  id: number;
+  title: string;
+  year: number;
+  genre: string;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const typeDefs = gql`
   type Movie {
-    id: Int!
+    id: ID!
     title: String!
-    year: Int
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
-    deleteMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
+    updateMovie(id: Int!, title: String, year: Int, genre: String): Movie
+    deleteMovie(id: Int!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [
-      { id: 1, title: "Hello", year: 2021 },
-      { id: 2, title: "Hello", year: 2022 },
-    ],
-    movie: () => ({ id: 1, title: "Hello", year: 2021 }),
+    movies: () => client.movie.findMany(),
+    movie: () => client.movie.findFirst(),
   },
   Mutation: {
-    createMovie: (_: any, { title }: { title: string }): boolean => {
-      console.log(title);
-      return true;
-    },
-    deleteMovie: (_: any, { title }: { title: string }): boolean => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_: any, { title, year, genre }: Movie) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    updateMovie: (_: any, { id, title, year, genre }: Movie) =>
+      client.movie.update({
+        where: { id },
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
+    deleteMovie: (_: any, { id }: Movie) =>
+      client.movie.delete({ where: { id } }),
   },
 };
 
