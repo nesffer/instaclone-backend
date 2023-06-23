b695ffc8606d380254c99c428e22a617e69269ae
Nesffer <nesffer.jimin@gmail.com>
2023-06-24 00:09:33 +0900

    refactor: 모듈화한 Movies TypeDefs, Resolvers 통합

---
+++ src/schema.ts
@@ -0,0 +1,14 @@
+import { loadFilesSync } from "@graphql-tools/load-files";
+import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+
+const loadedTypeDefs = loadFilesSync(`${__dirname}/**/*.typeDefs.ts`);
+const loadedResolvers = loadFilesSync(
+  `${__dirname}/**/*.{queries,mutations}.ts`
+);
+const typeDefs = mergeTypeDefs(loadedTypeDefs);
+const resolvers = mergeResolvers(loadedResolvers);
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export default schema;
