ab197895b3c9d92671a6b3d4ab93c88ba9931466
Nesffer <nesffer.jimin@gmail.com>
2023-06-24 00:10:09 +0900

    refactor: 통합된 Movies 스키마 적용

--- src/server.ts
+++ src/server.ts
@@ -1,69 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
-const client = new PrismaClient();
-
-interface Movie {
-  id: number;
-  title: string;
-  year: number;
-  genre: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-const typeDefs = gql`
-  type Movie {
-    id: ID!
-    title: String!
-    year: Int!
-    genre: String
-    createdAt: String!
-    updatedAt: String!
-  }
-  type Query {
-    movies: [Movie]
-    movie: Movie
-  }
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    updateMovie(id: Int!, title: String, year: Int, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: () => client.movie.findFirst(),
-  },
-  Mutation: {
-    createMovie: (_: void, { title, year, genre }: Movie) =>
-      client.movie.create({
-        data: {
-          title,
-          year,
-          genre,
-        },
-      }),
-    updateMovie: (_: void, { id, title, year, genre }: Movie) =>
-      client.movie.update({
-        where: { id },
-        data: {
-          title,
-          year,
-          genre,
-        },
-      }),
-    deleteMovie: (_: void, { id }: Movie) =>
-      client.movie.delete({ where: { id } }),
-  },
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+const server = new ApolloServer({ schema });
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
