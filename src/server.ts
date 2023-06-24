c549242375f56ac6e85ee47046b93f3fedd9d6aa
Nesffer <nesffer.jimin@gmail.com>
2023-06-24 16:06:44 +0900

    feat: .env 파일을 통해 환경변수를 불러올 수 있게 함

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 
 const server = new ApolloServer({ schema });
 
-server.listen().then(({ url }) => {
+const PORT: number = parseInt(process.env.PORT || "4000");
+
+server.listen(PORT).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
